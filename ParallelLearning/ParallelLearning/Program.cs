eb8de6397c08c34d4dcea7b54f4863a439624788
Paul Wang <1043469124@qq.com>
2019-09-03 23:09:13 +0800

    update some info of sync

--- ParallelLearning/ParallelLearning/Program.cs
+++ ParallelLearning/ParallelLearning/Program.cs
@@ -12,6 +12,10 @@ namespace ParallelLearning
         {
             //微软官方帮助文档：https://docs.microsoft.com/en-us/azure/devops/repos/git/gitquickstart?view=azure-devops&tabs=visual-studio
             //如何增加Github用你的Visual Studio:https://www.youtube.com/watch?v=Nw2tfO44_9E
+            //同步-同步：是指拉取远程仓库的更改（Fetch），然后尝试将这些更改添加到您本地的分支中。同时将保留本地修改的历史纪录.合并的挑战是从fetch与您的分支上的现有未刷新提交的冲突中获取提交。Git通常非常聪明地自动解决合并冲突，
+            //     但有时您必须手动解决合并冲突并使用新的合并提交完成合并。
+            //同步-提取：仅仅是拉取远程仓库的更改到窗体上，是否合并需要你审阅后决定
+            //同步-拉取：简单粗暴，直接将远程仓库的更改添加到你的本地分支，直接将你的本地分支与远程保持一致
         }
     }
 }
