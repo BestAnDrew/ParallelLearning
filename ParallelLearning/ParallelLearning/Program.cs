916a14186f593346e39d2b45445a7e1cf8cbbc61
Paul Wang <1043469124@qq.com>
2019-09-18 00:11:00 +0800

    Thread

--- ParallelLearning/ParallelLearning/Program.cs
+++ ParallelLearning/ParallelLearning/Program.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ParallelLearning
 {
     class Program
     {
+        static int count = 0;
+
+        static object baton = new object();
         static void Main(string[] args)
         {
             //微软官方帮助文档：https://docs.microsoft.com/en-us/azure/devops/repos/git/gitquickstart?view=azure-devops&tabs=visual-studio
@@ -16,6 +20,61 @@ namespace ParallelLearning
             //     但有时您必须手动解决合并冲突并使用新的合并提交完成合并。
             //同步-提取：仅仅是拉取远程仓库的更改到窗体上，是否合并需要你审阅后决定
             //同步-拉取：简单粗暴，直接将远程仓库的更改添加到你的本地分支，直接将你的本地分支与远程保持一致
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    var thread = new Thread(Different);
+            //    thread.Start(i);
+            //}
+            //Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            //for (int i = 0; i < Environment.ProcessorCount; i++)
+            //{
+            //    var thread = new Thread(Different);
+            //    thread.IsBackground = true;
+            //    thread.Start(i);
+            //}
+
+
+            //Console.WriteLine("Leaving main");
+            //Console.Read();
+
+            var thread1 = new Thread(IncrementCount);
+            var thread2 = new Thread(IncrementCount);
+
+
+            bool lockToken = false;
+            Monitor.Enter(baton,ref lockToken);
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if(lockToken)
+                    Monitor.Exit(baton);
+
+            }
+
+        }
+
+
+        static void IncrementCount()
+        {
+            while (true)
+            {
+                count++;
+                Console.WriteLine("ThreadID = "+Thread.CurrentThread.ManagedThreadId
+                                  +"Increment Count to = " + count);
+            }
+        }
+        static void Different(Object ThreadID)
+        {
+            while (true)
+                Console.WriteLine("hello from Different Method" + Thread.CurrentThread.ManagedThreadId);
         }
     }
 }
